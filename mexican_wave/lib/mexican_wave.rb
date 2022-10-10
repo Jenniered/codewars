6a6991a8afcfce1e8ece8a0097c5012041ccab99
jennieevans <jennie1evans@hotmail.com>
2022-10-10 15:09:17 +0100

    three letter input tests passing

--- mexican_wave/lib/mexican_wave.rb
+++ mexican_wave/lib/mexican_wave.rb
@@ -1,3 +1,17 @@
 def mexican_wave(str)
-  str.upcase.split("")
+  arr = str.split("")
+  if str.length === 1
+    first = "#{arr[0].upcase}"
+    [first]
+    # str.upcase.split("") 
+  elsif str.length === 2
+    first = "#{arr[0].upcase}#{arr[1]}"
+    second = "#{arr[0]}#{arr[1].upcase}"
+    [first, second]  
+ else
+    first = "#{arr[0].upcase}#{arr[1]}#{arr[2]}"
+    second = "#{arr[0]}#{arr[1].upcase}#{arr[2]}"
+    third = "#{arr[0]}#{arr[1]}#{arr[2].upcase}"
+    [first, second, third]  
+ end
 end
--- mexican_wave/spec/mexican_wave_spec.rb
+++ mexican_wave/spec/mexican_wave_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe "mexican wave method" do
   it "returns array for one letter input j" do
     expect(mexican_wave("j")).to eq ["J"]
   end
+
+  it "returns array for two letter input it" do
+    expect(mexican_wave("it")).to eq ["It", "iT"]
+  end
+
+  it "returns array for two letter input to" do
+    expect(mexican_wave("to")).to eq ["To", "tO"]
+  end
+
+  it "returns array for three letter input two" do
+    expect(mexican_wave("two")).to eq ["Two", "tWo", "twO"]
+  end
 end
