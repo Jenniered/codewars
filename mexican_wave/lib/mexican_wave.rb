de4ff758d31596ac0dc0fd0225130b604dcc6b5a
jennieevans <jennie1evans@hotmail.com>
2022-10-10 17:21:52 +0100

    refactor deleting of white space string

--- mexican_wave/lib/mexican_wave.rb
+++ mexican_wave/lib/mexican_wave.rb
@@ -1,6 +1,6 @@
 def mexican_wave(str)
   arr = ((str + ",") * str.length).split(",")
-  wave_arr = arr.each_with_index.map do |word, index|
+  split_arr = arr.each_with_index.map do |word, index|
     word.chars.each_with_index.map do |char, i|
       if i == index
         char.upcase
@@ -9,13 +9,8 @@ def mexican_wave(str)
       end
     end
 end
-  new_arr = wave_arr.map do |arr|
+  wave_arr = split_arr.map do |arr|
     arr.join("")
   end
-  if new_arr.include? str
-    new_arr.delete(str)
-    new_arr
-  else
-    new_arr
-  end
+  wave_arr - [str]
 end
