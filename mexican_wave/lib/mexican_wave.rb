1d40c814276591ff4b0cb286c6e296ee4940404c
jennieevans <jennie1evans@hotmail.com>
2022-10-11 10:30:37 +0100

    refactor - alternative method

--- mexican_wave/lib/mexican_wave.rb
+++ mexican_wave/lib/mexican_wave.rb
@@ -1,16 +1,18 @@
 def mexican_wave(str)
   arr = ((str + ",") * str.length).split(",")
   split_arr = arr.each_with_index.map do |word, index|
-    word.chars.each_with_index.map do |char, i|
-      if i == index
-        char.upcase
-      else
-        char 
-      end
-    end
-end
-  wave_arr = split_arr.map do |arr|
-    arr.join("")
+    word[0...index] + word[index].upcase + word[index+1..-1]
   end
-  wave_arr - [str]
+#     word.chars.each_with_index.map do |char, i|
+#       if i == index
+#         char.upcase
+#       else
+#         char 
+#       end
+#     end
+# end
+#   wave_arr = split_arr.map do |arr|
+#     arr.join("")
+#   end
+  split_arr - [str]
 end
