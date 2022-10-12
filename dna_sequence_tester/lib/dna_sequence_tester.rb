49159ce0d2d4ae261ef7661a9b40928e34b4505b
jennieevans <jennie1evans@hotmail.com>
2022-10-12 17:44:04 +0100

    single sequence with G and T

--- dna_sequence_tester/lib/dna_sequence_tester.rb
+++ dna_sequence_tester/lib/dna_sequence_tester.rb
@@ -1,3 +1,3 @@
 def dna_sequence_tester(seq1, seq2)
-  seq2 == 'C'
+  seq1 == 'G' && seq2 == 'C' || seq2 == 'G' && seq1 == 'C'
 end
--- dna_sequence_tester/spec/dna_sequence_tester_spec.rb
+++ dna_sequence_tester/spec/dna_sequence_tester_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe "dna sequence tester method" do
     expect(dna_sequence_tester('G', 'C')).to eq true
   end
 
-  it "returns true for single sequences 'T' and 'A'" do
+  it "returns false for single sequences 'G' and 'T'" do
     expect(dna_sequence_tester('G', 'T')).to eq false
+  end
+
+  it "returns false for single sequences 'T' and 'C'" do
+    expect(dna_sequence_tester('T', 'C')).to eq false
+  end
+
+  it "returns true for single sequences 'C' and 'G'" do
+    expect(dna_sequence_tester('C', 'G')).to eq true
   end
 end
