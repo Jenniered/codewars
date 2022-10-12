0bebf6d0cecf3ef20d55e57653825c2735af1457
jennieevans <jennie1evans@hotmail.com>
2022-10-12 18:45:06 +0100

    refactor to remove single letter method

--- dna_sequence_tester/lib/dna_sequence_tester.rb
+++ dna_sequence_tester/lib/dna_sequence_tester.rb
@@ -1,13 +1,4 @@
 def dna_sequence_tester(seq1, seq2)
-  if seq1.length == 1
-    if seq1 == 'G' && seq2 == 'C' || seq2 == 'G' && seq1 == 'C'
-      true
-    elsif seq1 == 'A' && seq2 == 'T' || seq2 == 'A' && seq1 == 'T'
-      true
-    else
-      false
-  end
-else
   seq1_arr = seq1.split(//)
   seq2_arr = seq2.reverse.split(//)
 
@@ -21,5 +12,4 @@ else
     end
   end
   !new_arr.include? false
-end
 end
