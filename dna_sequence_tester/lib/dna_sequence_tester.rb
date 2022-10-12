215b0c3c65d57e06b91cfb197a555afd0cbd3101
jennieevans <jennie1evans@hotmail.com>
2022-10-12 18:27:05 +0100

    tests passing for two digit strings with T and A

--- dna_sequence_tester/lib/dna_sequence_tester.rb
+++ dna_sequence_tester/lib/dna_sequence_tester.rb
@@ -1,9 +1,19 @@
 def dna_sequence_tester(seq1, seq2)
-  if seq1 == 'G' && seq2 == 'C' || seq2 == 'G' && seq1 == 'C'
-    true
-  elsif seq1 == 'A' && seq2 == 'T' || seq2 == 'A' && seq1 == 'T'
-    true
-  else
-    false
+  if seq1.length == 1
+    if seq1 == 'G' && seq2 == 'C' || seq2 == 'G' && seq1 == 'C'
+      true
+    elsif seq1 == 'A' && seq2 == 'T' || seq2 == 'A' && seq1 == 'T'
+      true
+    else
+      false
   end
+else
+  seq1_arr = seq1.split(//)
+  seq2_arr = seq2.reverse.split(//)
+
+  new_arr = seq1_arr.each_with_index.map do |char1, index|
+    char1 == 'T' && seq2_arr[index] == 'A' || char1 == 'A' && seq2_arr[index] == 'T'
+  end
+  !new_arr.include? false
+end
 end
--- dna_sequence_tester/spec/dna_sequence_tester_spec.rb
+++ dna_sequence_tester/spec/dna_sequence_tester_spec.rb
@@ -28,4 +28,12 @@ RSpec.describe "dna sequence tester method" do
   it "returns true for single sequences 'T' and 'A'" do
     expect(dna_sequence_tester('T', 'A')).to eq true
   end
+
+  it "returns true for sequences 'TA' and 'TA'" do
+    expect(dna_sequence_tester('TA', 'TA')).to eq true
+  end
+
+  it "returns false for sequences 'TA' and 'AA'" do
+    expect(dna_sequence_tester('TA', 'AA')).to eq false
+  end
 end
