01ece362a8a3082ae876a5b46561aaa66309b60d
jennieevans <jennie1evans@hotmail.com>
2022-10-12 17:34:55 +0100

    true and false one letter string input

--- dna_sequence_tester/lib/dna_sequence_tester.rb
+++ dna_sequence_tester/lib/dna_sequence_tester.rb
@@ -1,3 +1,3 @@
 def dna_sequence_tester(seq1, seq2)
-  true
+  seq2 == 'C'
 end
--- dna_sequence_tester/spec/dna_sequence_tester_spec.rb
+++ dna_sequence_tester/spec/dna_sequence_tester_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe "dna sequence tester method" do
   it "returns true for single sequences 'G' and 'C'" do
     expect(dna_sequence_tester('G', 'C')).to eq true
   end
+
+  it "returns true for single sequences 'T' and 'A'" do
+    expect(dna_sequence_tester('G', 'T')).to eq false
+  end
 end
