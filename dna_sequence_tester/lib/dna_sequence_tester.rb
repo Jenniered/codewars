6b76d6e50ec080fe4b442850beaf52a946d9920e
jennieevans <jennie1evans@hotmail.com>
2022-10-12 17:49:30 +0100

    tests passing for single strands for CG and AT

--- dna_sequence_tester/lib/dna_sequence_tester.rb
+++ dna_sequence_tester/lib/dna_sequence_tester.rb
@@ -1,3 +1,9 @@
 def dna_sequence_tester(seq1, seq2)
-  seq1 == 'G' && seq2 == 'C' || seq2 == 'G' && seq1 == 'C'
+  if seq1 == 'G' && seq2 == 'C' || seq2 == 'G' && seq1 == 'C'
+    true
+  elsif seq1 == 'A' && seq2 == 'T' || seq2 == 'A' && seq1 == 'T'
+    true
+  else
+    false
+  end
 end
--- dna_sequence_tester/spec/dna_sequence_tester_spec.rb
+++ dna_sequence_tester/spec/dna_sequence_tester_spec.rb
@@ -16,4 +16,16 @@ RSpec.describe "dna sequence tester method" do
   it "returns true for single sequences 'C' and 'G'" do
     expect(dna_sequence_tester('C', 'G')).to eq true
   end
+
+  it "returns true for single sequences 'A' and 'T'" do
+    expect(dna_sequence_tester('A', 'T')).to eq true
+  end
+
+  it "returns false for single sequences 'A' and 'G'" do
+    expect(dna_sequence_tester('A', 'G')).to eq false
+  end
+
+  it "returns true for single sequences 'T' and 'A'" do
+    expect(dna_sequence_tester('T', 'A')).to eq true
+  end
 end
