27476e842f6466ffe61089e80587db21487f2466
jennieevans <jennie1evans@hotmail.com>
2022-10-13 12:02:18 +0100

    refactor

--- narcisstic_number/lib/narcisstic_number.rb
+++ narcisstic_number/lib/narcisstic_number.rb
@@ -1,7 +1,6 @@
 def narcisstic_number?(num)
-  digits_count = num.digits.length
     power_arr = num.digits.map do 
-      |n| n ** digits_count
+      |n| n ** num.digits.length
     end
   num == power_arr.sum
 end
