8c75eb5a2375aec9dca21b845931d2d2992a9853
jennieevans <jennie1evans@hotmail.com>
2022-10-13 11:06:19 +0100

    layout and variable names amended

--- narcisstic_number/lib/narcisstic_number.rb
+++ narcisstic_number/lib/narcisstic_number.rb
@@ -1,7 +1,7 @@
 def narcisstic_number?(num)
-  num_of_digits = num.digits.length
-    power_arr = num.digits.map do |n| 
-      n**num_of_digits
-      end
-    num == power_arr.sum
+  digits_count = num.digits.length
+    power_arr = num.digits.map do 
+      |n| n ** digits_count
+    end
+  num == power_arr.sum
 end
