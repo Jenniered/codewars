579afc46d7af4629c83ed08ac53c818855b50d49
jennieevans <jennie1evans@hotmail.com>
2022-10-13 16:55:12 +0100

    tidied up final solution

--- roman_numerals_decoder/lib/roman_numerals_decoder.rb
+++ roman_numerals_decoder/lib/roman_numerals_decoder.rb
@@ -8,23 +8,16 @@ def roman_numerals_decoder(roman)
     "D" => 500,
     "M" => 1000
   }
-  array = roman.chars
-  values_arr = array.each_with_index.map do |char, index|
-    # next_char = array[index + 1]
-    # if values[char] < values[next_char] 
-    #   -1 * values[char]
-    # else
+
+  values_arr = roman.chars.map do |char|
       values[char]
     end
-  # end
-  # [-100, 500, -10, 50, -1, 5]
-  adj_arr = values_arr.each_cons(2).map do |a, b|
+  adjusted_arr = values_arr.each_cons(2).map do |a, b|
     if a < b
       -1 * a
     else
       a      
     end
   end
-  p adj_arr
-  adj_arr.push(values_arr[-1]).sum
+  adjusted_arr.push(values_arr[-1]).sum
 end
