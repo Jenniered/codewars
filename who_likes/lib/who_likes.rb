11c58b3d5db7ea03c209afc68db4281e65554d91
jennieevans <jennie1evans@hotmail.com>
2022-10-31 17:20:55 +0000

    three passing tests with if statements

--- who_likes/lib/who_likes.rb
+++ who_likes/lib/who_likes.rb
@@ -1,6 +1,8 @@
 def who_likes(array)
   if array == ["Peter"]
     "Peter likes this"
+  elsif array == ["John"]
+    "John likes this"
   else 
     "no one likes this"
   end
--- who_likes/spec/who_likes_spec.rb
+++ who_likes/spec/who_likes_spec.rb
@@ -2,10 +2,14 @@ require 'who_likes'
 
 RSpec.describe "who likes method" do
   it "returns 'no one' for an empty array" do
-  expect(who_likes([])).to eq "no one likes this"
+    expect(who_likes([])).to eq "no one likes this"
   end
 
   it "returns the name for an array with single name" do
     expect(who_likes(["Peter"])).to eq "Peter likes this"
-    end
+  end
+  
+  it "returns the name for an array with single name - John" do
+    expect(who_likes(["John"])).to eq "John likes this"
+  end
 end
