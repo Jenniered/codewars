47faf9eb37b9b42291966bf40750a415e7a16383
jennieevans <jennie1evans@hotmail.com>
2022-11-16 12:51:46 +0000

    bcc - second test passing

--- break_camel_case/lib/break_camel_case.rb
+++ break_camel_case/lib/break_camel_case.rb
@@ -1,3 +1,7 @@
 def break_camel_case(string)
-  ""
+  if string == "string"
+    "string"
+  else
+    ""
+  end
 end
--- break_camel_case/spec/break_camel_case_spec.rb
+++ break_camel_case/spec/break_camel_case_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe "break camel case method" do
   it 'returns an empty string when empty string input' do
     expect(break_camel_case("")).to eq ""
   end
+
+  it 'returns the string when all lower case' do
+    expect(break_camel_case("string")).to eq "string"
+  end
 end
